abe869d432e211259f108f25feaa8409058bbf76
Massny <pawel.fornagiel2@gmail.com>
2024-07-15 23:04:09 +0200

    move environmental usage to run

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
 from app.DexcomApi import DexcomApi
 from app.Widget import Widget
+# Environmental variables
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
+USERNAME = os.getenv('USERNAME')
+PASSWORD = os.getenv('PASSWORD')
 # Testing initialisation
-dex_api = DexcomApi(True)
+dex_api = DexcomApi(True, USERNAME, PASSWORD)
 test = Widget(dex_api)
