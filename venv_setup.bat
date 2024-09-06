56a788ec44a0afe4f49352ed9dfc81d9db7793a4
Massny <pawel.fornagiel2@gmail.com>
2024-09-07 00:00:22 +0200

    add venv setup

---
+++ venv_setup.bat
@@ -0,0 +1,8 @@
+:: Create the virtual environment
+python -m venv .venv
+
+:: Activate the virtual environment
+call .venv\Scripts\activate
+
+:: Install dependencies from requirements.txt
+pip install -r requirements.txt
