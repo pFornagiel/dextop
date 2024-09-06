36a3456ddf3b44f7928e71d41999e5fc4eae17cd
Massny <pawel.fornagiel2@gmail.com>
2024-09-07 00:00:34 +0200

    update run code

--- run.py
+++ run.py
@@ -1,13 +1,3 @@
-from app.DexcomApi import DexcomApi
-from app.Widget import Widget
-# from app.Setup import SetupWindow
-# Environmental variables
-import os
-from dotenv import load_dotenv
-load_dotenv()
+from app.Setup import SetupWindow
 
-USERNAME = os.getenv('_USERNAME')
-PASSWORD = os.getenv('_PASSWORD')
-# Testing initialisation
-dex_api = DexcomApi(True, USERNAME, PASSWORD)
-test = Widget(dex_api)
+SetupWindow()
