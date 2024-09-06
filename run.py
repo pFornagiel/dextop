251df7e3c9095172b8c5ecae1d1fb510df71c145
Massny <pawel.fornagiel2@gmail.com>
2024-09-06 23:07:06 +0200

    fix .env constants

--- run.py
+++ run.py
@@ -1,12 +1,13 @@
 from app.DexcomApi import DexcomApi
 from app.Widget import Widget
+# from app.Setup import SetupWindow
 # Environmental variables
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
-USERNAME = os.getenv('USERNAME')
-PASSWORD = os.getenv('PASSWORD')
+USERNAME = os.getenv('_USERNAME')
+PASSWORD = os.getenv('_PASSWORD')
 # Testing initialisation
 dex_api = DexcomApi(True, USERNAME, PASSWORD)
 test = Widget(dex_api)
