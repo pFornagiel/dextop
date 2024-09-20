15ac3d005493c60c13a6a40d64241ebe9c8049b3
Massny <pawel.fornagiel2@gmail.com>
2024-09-20 16:26:57 +0200

    fix venv setup script

--- scripts/venv_setup.bat
+++ scripts/venv_setup.bat
@@ -2,7 +2,7 @@
 python -m venv .venv
 
 :: Activate the virtual environment
-call .venv\Scripts\activate
+CALL ".venv\Scripts\activate" 
 
 :: Install dependencies from requirements.txt
-pip install -r requirements.txt
+".venv\Scripts\python.exe" -m pip install -r requirements.txt
