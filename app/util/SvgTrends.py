c252893d1417bbd837bd706d4b01fcd92056361a
Massny <pawel.fornagiel2@gmail.com>
2025-02-08 14:45:03 +0100

    Change SVG handling, add SVG files

---
+++ app/util/SvgTrends.py
@@ -0,0 +1,23 @@
+# Util
+import os
+# Config
+from app.Consts import TREND_SVG_PATH
+
+TREND_LIST: list[str] = []
+
+def load_svgs():
+  global TREND_LIST
+  for i in range(len(os.listdir(TREND_SVG_PATH))):
+    with open(os.path.join(TREND_SVG_PATH, f'trend_{i}.svg'), 'r') as file:
+      TREND_LIST.append(file.read())
+      
+
+def get_trend_SVG(number: int, colour: str, size: int) -> str:
+  if number == 0:
+    return TREND_LIST[0]
+  if number == 7 or number == 1:
+    return TREND_LIST[number].format(size, size, colour, colour)
+  return TREND_LIST[number].format(size, size, colour)
+
+# Load SVGs on startup
+load_svgs()
